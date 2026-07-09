6379c886000e6a1f48e3ed4e70151877240b8ce7
Fiijoey <kofimensah3730@gmail.com>
2026-07-09 21:15:24 +0000

    Model

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8072206b5b46f0e41e181e6ede2d9b61c1ee687d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98ccc5d238edf12135eea812ac2d71c8c6a2b60a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
