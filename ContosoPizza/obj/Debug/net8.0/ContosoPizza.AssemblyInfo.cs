5cf4e644d571be93c81392795cab2a546cb57ef6
Fiijoey <kofimensah3730@gmail.com>
2026-07-10 17:56:48 +0000

    Update Movie model with validation attributes and modify database context

--- ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9edf760abe8fdf8d4768ec6db70baabb1006c158")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+552a78c260b499948453d211942a75706cdf3b46")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+967a5780715ab3e3c3574dd64b3816d80a6c8d29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+552a78c260b499948453d211942a75706cdf3b46")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
