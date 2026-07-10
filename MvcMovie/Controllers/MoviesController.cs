6f45f0c4fcb01af0116465c3b183b02b97773c8d
Fiijoey <kofimensah3730@gmail.com>
2026-07-10 19:29:02 +0000

    Add year filter to Movies index view and controller for enhanced search functionality

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -20,34 +20,49 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string movieGenre, string searchString)
+        public async Task<IActionResult> Index(string movieGenre, string searchString, int? yearFilter)
         {
             if (_context.Movie == null)
             {
-                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+                return Problem("Entity set 'MvcMovieContext.Movie' is null.");
             }
 
             // Use LINQ to get list of genres.
             IQueryable<string> genreQuery = from m in _context.Movie
                                             orderby m.Genre
                                             select m.Genre;
+
             var movies = from m in _context.Movie
                          select m;
 
+            // Filter by title
             if (!string.IsNullOrEmpty(searchString))
             {
                 movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
             }
 
+            // Filter by genre
             if (!string.IsNullOrEmpty(movieGenre))
             {
                 movies = movies.Where(x => x.Genre == movieGenre);
             }
 
+            // Filter by year
+            if (yearFilter.HasValue)
+            {
+                movies = movies.Where(m => m.ReleaseDate.Year >= yearFilter.Value);
+            }
+
+            // Populate genres for the dropdown
+            var genres = await genreQuery.Distinct().ToListAsync();
+
             var movieGenreVM = new MovieGenreViewModel
             {
-                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
-                Movies = await movies.ToListAsync()
+                Movies = await movies.ToListAsync(),
+                Genres = new SelectList(genres),
+                MovieGenre = movieGenre,
+                SearchString = searchString,
+                YearFilter = yearFilter
             };
 
             return View(movieGenreVM);
--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cf4e644d571be93c81392795cab2a546cb57ef6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a326465310091e8503ce50eb35d3c810896915")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
